2ff93edc2da93c3ffa668677785ddecb0ed133d2
delaMorena <martadmdo@gmail.com>
2021-05-22 21:12:54 +0000

    css

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,20 +8,17 @@ export const Home = () => {
 
 	return (
 		<div>
-			<div className="row">
-				<div className="col-6 pl-5 pt-5 d-flex align-content-center flex-wrap">
-					<div>
-						<h1>Eres del Valle de Tena </h1>
-						<h3>Resides en algún pueblo de la Comarca Alto-Gállego</h3>
-					</div>
-					<div>
-						<h1>Te gusta leer </h1>
-						<h3>Pero todas las bibliotecas están cerradas o sus horarios no te cuadran...</h3>
-					</div>
+			<div className="bg-mountains">
+				<div>
+					<h1>Eres del Valle de Tena </h1>
+					<h1>Te gusta leer </h1>
+					<h4>Resides en algún pueblo de la Comarca Alto-Gállego</h4>
+					<h4>Pero todas las bibliotecas están cerradas o sus horarios no te cuadran...</h4>
 				</div>
-				<div className="bg-mountains col-6" />
+				<div />
 			</div>
-			<div className="mt-5 row justify-content-center bg-books">
+
+			<div className="mt-1 row justify-content-center bg-books">
 				<div className="col-4 pseudomodal">
 					<h2>¿Como funciona libros trashumantes?</h2>
 				</div>
